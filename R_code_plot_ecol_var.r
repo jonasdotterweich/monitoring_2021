476064b2c244de6b3278e32f6def0beecf8292b5
jonasdotterweich <73693995+jonasdotterweich@users.noreply.github.com>
2020-11-06 11:06:46 +0100

    Create R_code_plot_ecol_var.r

---
+++ R_code_plot_ecol_var.r
@@ -0,0 +1,53 @@
+# second lecture on R coding 06.11.20: plotting relationship among ecological variables
+
+#first of all we were installin sthe spatial datatset package by install.package("sp")
+#this is what R showed: install.packages("sp")
+#--- Bitte einen CRAN Spiegel für diese Sitzung auswählen ---
+#versuche URL 'https://ftp.fau.de/cran/bin/macosx/contrib/4.0/sp_1.4-4.tgz'
+#Content type 'application/x-gzip' length 1932488 bytes (1.8 MB)
+#==================================================
+#downloaded 1.8 MB
+
+
+#Die heruntergeladenen Binärpakete sind in 
+	#    /var/folders/k6/wprrl8j51117yskvlzgvx9d40000gn/T//RtmpbVX2tW/downloaded_packages
+
+
+library(sp) #require() function could do the same
+
+#data is used to recall datasets
+data(meuse)
+#the look isnide the dataset
+meuse
+
+# view the datatset (prpoerly) attention: Capital letter needed
+View(meuse)
+#View is not working for mac
+
+#to see the first lines of dataset
+head(meuse)
+
+summary(meuse)
+
+plot(cadmium,zink)
+#error for that function, object names not found
+
+# solving the error by linking the name to the datatset with the dollar sign $
+plot(meuse$cadmium, meuse$zinc)
+
+# yet rather than using the dolla sign we can use an attach function, then from this point on it will permanetly be attached
+
+# with attach(meuse) the next orders/fuctions will be attached to the meuse data
+
+attach(meuse)   
+plot(cadmium, zinc)
+
+plot(cadmium, lead)
+
+#how to impress your supervisor
+# to create a scatterplot matricies
+pairs(meuse)
+
+#Question/excersise for the weekend:
+#pairing only some parts of the dataset not the total one
+#pairing only cadmium, copper, lead, zinc
