38ed36645632af5fb9b23e7e0aea05e6bdf5e723
jonasdotterweich <73693995+jonasdotterweich@users.noreply.github.com>
2020-11-16 10:58:23 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -27,3 +27,48 @@ bubble(meuse, "zinc")
 #do the asme for lead
 bubble(meuse, "lead")
 bubble(meuse, "lead", col="orange")
+
+
+# second lesson using ggplot2 - 16.11.20
+# ggplot2 was installed by:
+install.pckages("ggplot2")
+library(ggplot2)
+
+#made up ecological plot
+#biofules -- antioxidants enzymes
+
+biofules<- c(120, 200, 350, 570, 750)   #array of values by c
+
+oxidative<- c(1200, 1300, 21000, 34000, 50000)
+
+#creating a table by data.frame named d
+d<- data.frame(biofules, oxidative)
+#its the same to do a dataframe ith ggplot
+ggplot(d, aes(x=biofules, y=oxidative))+geom_point()
+
+#mofifiying the expression of the points
+ggplot(d, aes(x=biofules, y=oxidative))+geom_point(,col="red", size=5)
+
+#instead of pints we can use a line as well by geom_line
+ggplot(d, aes(x=biofules, y=oxidative))+geom_line()
+
+# we can mix things
+ggplot(d, aes(x=biofules, y=oxidative))+geom_line()+geom_point(,size=3)
+
+#polygons
+ggplot(d, aes(x=biofules, y=oxidative))+geom_polygon()
+
+       ##           ##
+   ######           #####
+#########Import Data########
+   ######           #####
+       ##           ##
+
+setwd("path/lab")
+
+#work directory for mac (need the path)
+setwd("/Users/JD/lab")
+
+# importing the table
+covid<-read.table("covid_agg.csv", header=TRUE)
+covid
