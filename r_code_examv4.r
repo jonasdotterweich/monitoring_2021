d4aa7cddf42b03fdabbce2c36482f68dc34b1207
jonasdotterweich <73693995+jonasdotterweich@users.noreply.github.com>
2021-02-25 20:21:56 +0100

    Update r_code_examv4.r

--- r_code_examv4.r
+++ r_code_examv4.r
@@ -1,11 +1,27 @@
-#new idea for australian fires project would be using sentinel two data
-#but first we need to try how to make it work
+# idea for exam plot nox data for germany and see how it changed with the diesel
+  
+library(ncdf4)
+setwd("/Users/JD/lab/exam/") #new wd for the exam (exam folder in lab folder)
+library(raster)
+library(rgdal)
+  
+nox_18<-stack("CAMS-GLOB-ANT_v4.2_nox_2018.nc")
+plot(nox_18)
 
-library(maps)
-ImageCollection('COPERNICUS/S2_SR')
-function maskS2clouds(image) {
-  var qa = image.select('QA60')
-  
-  #okay, no chance, I know too little about it, dont even know if it could work with r
-  
-  
+nox_18
+
+nox_19<-stack("CAMS-GLOB-ANT_v4.2_nox_2019.nc")
+plot(nox_19)
+
+nox_20<-stack("CAMS-GLOB-ANT_v4.2_nox_2020.nc")
+plot(nox_20)
+
+#trying something else like here:
+
+library(raster)
+library(rgdal)
+library(rasterVis)
+library(RColorBrewer)
+setwd("/Users/JD/lab/exam/")
+nox_18<-stack("CAMS-GLOB-ANT_v4.2_nox_2018.nc")
+nox_18
